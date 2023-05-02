d227d85c7d644e0455a5235080ba6c6e5269d2fa
cal-brynestad <cabr8942@colorado.edu>
2023-05-02 13:06:10 -0600

    fixed f32 matrix graph

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-fileToRead = "data/f64triangularInv.csv"
+fileToRead = "data/f32matrix.csv"
 Data = pd.read_csv(fileToRead)
 
-st.write("Performance of computing A^-1 where A is a square triangular matrix with dimension n (ns)")
+st.write("Performance of multiplication of two square matrices of dimension n (ns)")
 st.line_chart(data = Data)
 
 
