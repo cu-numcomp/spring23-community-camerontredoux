1624d415be215c93b54b2e87ea0c3662b16651be
cal-brynestad <cabr8942@colorado.edu>
2023-05-03 00:01:07 -0600

    models

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-fileToRead = "data/f32matrix.csv"
+fileToRead = "data/f64triangularSol.csv"
 Data = pd.read_csv(fileToRead)
 
-st.write("Performance of multiplication of two square matrices of dimension n (ns)")
+st.write("Solving AX = B in place where A and B are two square matrices of dimension n, and A is a triangular matrix (ns)")
 st.line_chart(data = Data)
 
 
